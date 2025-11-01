5db5ad94c30452a9a29f5fca254a9d93527fdd6c
Pedro-pb-coder <pedro.pe.madureira@gmail.com>
2025-11-01 15:59:36 -0300

    primeiro commit - Pedro

--- backend/app/main.py
+++ backend/app/main.py
@@ -136,3 +136,5 @@ def optimized_search_companies(
 
 # # if __name__ == "__main__":
 # #     uvicorn.run("backend.app.main:app", host="127.0.0.1", port=8000, reload=True)
+
+# primeira alteração - pedro 
